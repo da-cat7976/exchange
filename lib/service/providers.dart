e119bec90617d8a84f6eaeb7ffcd760c450e7552
da-cat7976 <gierr7976@gmail.com>
2025-09-09 18:01:08 +0300

    ref: Service providers

---
+++ lib/service/providers.dart
@@ -0,0 +1,33 @@
+import 'package:exchange/domain/currency.dart';
+import 'package:exchange/domain/rate.dart';
+import 'package:exchange/domain/source.dart';
+import 'package:exchange/logic/source.dart';
+import 'package:exchange/service/open_exchange_rates/api.dart';
+import 'package:exchange/service/open_exchange_rates/services.dart';
+import 'package:exchange/utils/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'providers.g.dart';
+
+@riverpod
+CurrencyService currencyService(Ref ref) {
+  final source = ref.watch(sourceControllerProvider);
+  final dio = ref.watch(dioProvider);
+
+  switch (source) {
+    case RateSource.openExchangeRates:
+      return OerCurrencyService(api: OpenExchangeRatesApi(dio));
+  }
+}
+
+@riverpod
+ExchangeRatesService exchangeRatesService(Ref ref) {
+  final source = ref.watch(sourceControllerProvider);
+  final dio = ref.watch(dioProvider);
+
+  switch (source) {
+    case RateSource.openExchangeRates:
+      return OerRatesService(api: OpenExchangeRatesApi(dio));
+  }
+}
