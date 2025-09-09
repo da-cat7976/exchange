1d2406cafa2a9216c24dd27dc9cc4e4d126f2a6d
da-cat7976 <gierr7976@gmail.com>
2025-09-09 15:11:28 +0300

    feat: Added app config

---
+++ lib/utils/env.dart
@@ -0,0 +1,25 @@
+import 'package:exchange/domain/source.dart';
+
+abstract class Env {
+  const Env._();
+
+  static const String openExchangeRatesApi = String.fromEnvironment('oerApi');
+
+  static const String openExchangeRatesAppId = String.fromEnvironment(
+    'oerAppId',
+  );
+
+  static const String _defaultSource = String.fromEnvironment('defaultSource');
+
+  static const String defaultFrom = String.fromEnvironment('defaultFrom');
+
+  static const String defaultTo = String.fromEnvironment('defaultTo');
+
+  static const int defaultAmount = int.fromEnvironment('defaultAmount');
+
+  static RateSource get defaultSource {
+    return RateSource.values.firstWhere(
+      (source) => source.name.toLowerCase() == _defaultSource.toLowerCase(),
+    );
+  }
+}
