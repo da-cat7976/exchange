b4f8974691804f3798757f1e71e35a8c3f204f75
da-cat7976 <gierr7976@gmail.com>
2025-09-09 15:29:14 +0300

    feat: API adapter for openexchangerates.org

---
+++ lib/service/open_exchange_rates/api.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:exchange/utils/env.dart';
+import 'package:exchange/utils/json.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'api.g.dart';
+
+@RestApi(baseUrl: Env.openExchangeRatesApi)
+abstract interface class OpenExchangeRatesApi {
+  factory OpenExchangeRatesApi(Dio dio, {String? baseUrl}) =
+      _OpenExchangeRatesApi;
+
+  @GET('/currencies.json')
+  Future<Map<String, String>> getCurrencies({
+    @Query('app_id') String appId = Env.openExchangeRatesAppId,
+  });
+}
+
+@JsonSerializable(createToJson: false)
+final class OerRatesRsDto {
+  @unixDateTime
+  final DateTime timestamp;
+
+  final String base;
+
+  final Map<String, double> rates;
+
+  const OerRatesRsDto({
+    required this.timestamp,
+    required this.base,
+    required this.rates,
+  });
+
+  factory OerRatesRsDto.fromJson(Map<String, dynamic> json) =>
+      _$OerRatesRsDtoFromJson(json);
+}
